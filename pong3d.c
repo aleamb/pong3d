ad50768da7a6459a850f21382949e5d5d7f62519
aleamb <jandroz@gmail.com>
2017-10-30 12:51:02 +0100

    Clean code

--- pong3d.c
+++ pong3d.c
@@ -30,20 +30,15 @@ int ball_in_opponent_stick() {
 
 
 int ball_in_stick(float ball_x, float ball_y, float ball_width, PONG_ELEMENT* stick) {
-	return (
-			(((ball_x - ball_width) < (stick->x + stick->width2))
-			 &&
-			 ((ball_x + ball_width) > (stick->x - stick->width2)))
-			&&
-			(((ball_y - ball_width) < (stick->y + stick->height2))
-			 &&
-			 ((ball_y + ball_width) > (stick->y - stick->height2))));
+	return ((ball_x - ball_width) < (stick->x + stick->width2) &&
+			(ball_x + ball_width) > (stick->x - stick->width2) &&
+			(ball_y - ball_width) < (stick->y + stick->height2) &&
+			(ball_y + ball_width) > (stick->y - stick->height2));
 }
 
 void mouse_move_player_stick(int mx, int my) {
 
-	move_player_stick(
-			(mx - (WINDOW_WIDTH >> 1)) / (float)WINDOW_WIDTH,
-			//(aspet * h -> w/h * h -> h)
+	move_player_stick((mx - (WINDOW_WIDTH >> 1)) / (float)WINDOW_WIDTH,
+			//(aspect * h -> w/h * h -> h)
 			(my - (WINDOW_HEIGHT >> 1)) / -(float)WINDOW_WIDTH);
 }
